311a80a94c42a02796bfdcd0c2a82816151399ee
Proutman67 <ffffornite81@gmail.com>
2026-01-16 17:59:35 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,22 @@
+@echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: ---- Admin-only commands below ----
+echo Running as administrator
+pip install requests
+
+set URL=https://raw.githubusercontent.com/Proutman67/educational/refs/heads/main/installer.py
+set DEST=%~dp0installer.py
+
+powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%DEST%'"
+
+cd %~dp0
+python "%~dp0installer.py"
+
+pause
